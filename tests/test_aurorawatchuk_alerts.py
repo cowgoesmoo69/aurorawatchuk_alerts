b52461f1421914d7215b0ca4369e0d64df72a810
cowgoesmoo69 <earp.tom@gmail.com>
2026-01-30 15:04:04 +0000

    Split pre_checks() tests out individually

--- tests/test_aurorawatchuk_alerts.py
+++ tests/test_aurorawatchuk_alerts.py
@@ -4,67 +4,156 @@ from argparse import Namespace
 from app.aurorawatchuk_alerts import pre_checks, should_alert
 
 
-# pre_checks() tests
-def test_pre_checks():
-    # Variables for tests.
-    token = None
-    user = None
-    # Create a set of ArgumentParser args.
+# pre_checks() test
+def test_pre_checks_valid_data():
+    # Valid test data.
+    token = "abcdefghijklmnopqrstuvwxyz1234"
+    user = "abcdefghijklmnopqrstuvwxyz1234"
     args = Namespace(
-        threshold="bad",
-        alert_interval="bad",
-        check_interval="bad",
-        reduced_sensitivity=False,  # won't be tested as argparse controls True/False state.
-        ttl="bad",
+        threshold=1,
+        alert_interval=3600,
+        check_interval=300,
+        reduced_sensitivity=False,
+        ttl=14400,
     )
+    config = pre_checks(token, user, args)
+    assert config == {
+    "token": "abcdefghijklmnopqrstuvwxyz1234",
+    "user": "abcdefghijklmnopqrstuvwxyz1234",
+    "threshold": 1,
+    "alert_interval": 3600,
+    "check_interval": 300,
+    "reduced_sensitivity": False,
+    "ttl": 14400,
+    }
 
+
+def test_pre_checks_bad_token():
+    # Valid test data.
+    token = "abcdefghijklmnopqrstuvwxyz1234"
+    user = "abcdefghijklmnopqrstuvwxyz1234"
+    args = Namespace(
+        threshold=1,
+        alert_interval=3600,
+        check_interval=300,
+        reduced_sensitivity=False,
+        ttl=14400,
+    )
     # No token.
+    token = None
     with pytest.raises(
         RuntimeError, match="PUSHOVER_APP_TOKEN environment variable missing."
     ):
         config = pre_checks(token, user, args)
-    # Set a token, make it carry on.
+    # Invalid token.
     token = "moo"
+    with pytest.raises(
+        ValueError, match="PUSHOVER_APP_TOKEN format not valid. Only a-z, 0-9, 30 characters."
+    ):
+        config = pre_checks(token, user, args)
 
+
+def test_pre_checks_bad_user():
+    # Valid test data.
+    token = "abcdefghijklmnopqrstuvwxyz1234"
+    user = "abcdefghijklmnopqrstuvwxyz1234"
+    args = Namespace(
+        threshold=1,
+        alert_interval=3600,
+        check_interval=300,
+        reduced_sensitivity=False,
+        ttl=14400,
+    )
     # No user.
+    user = None
     with pytest.raises(
         RuntimeError, match="PUSHOVER_USER_KEY environment variable missing."
     ):
         config = pre_checks(token, user, args)
-    # Set a user, make it carry on.
+    # Invalid token.
     user = "moo"
+    with pytest.raises(
+        ValueError, match="PUSHOVER_USER_KEY format not valid. Only a-z, 0-9, 30 characters."
+    ):
+        config = pre_checks(token, user, args)
 
-    # Threshold not an integer.
+
+def test_pre_checks_bad_threshold():
+    # Valid test data.
+    token = "abcdefghijklmnopqrstuvwxyz1234"
+    user = "abcdefghijklmnopqrstuvwxyz1234"
+    args = Namespace(
+        threshold=1,
+        alert_interval=3600,
+        check_interval=300,
+        reduced_sensitivity=False,
+        ttl=14400,
+    )
+    # Threshold not an integer
+    args.threshold = "moo"
     with pytest.raises(TypeError, match="Threshold must be an integer."):
         config = pre_checks(token, user, args)
     # Threshold out of range.
     args.threshold = 5
     with pytest.raises(ValueError, match="Threshold must be between 1 and 3."):
         config = pre_checks(token, user, args)
-    # Set valid threshold, make it carry on.
-    args.threshold = 3
 
+
+def test_pre_checks_bad_alert_interval():
+    # Valid test data.
+    token = "abcdefghijklmnopqrstuvwxyz1234"
+    user = "abcdefghijklmnopqrstuvwxyz1234"
+    args = Namespace(
+        threshold=1,
+        alert_interval=3600,
+        check_interval=300,
+        reduced_sensitivity=False,
+        ttl=14400,
+    )
     # Alert interval not an integer.
+    args.alert_interval = "moo"
     with pytest.raises(TypeError, match="Alert interval must be an integer."):
         config = pre_checks(token, user, args)
     # Alert interval out of range.
     args.alert_interval = -1
     with pytest.raises(ValueError, match="Alert interval must be > 0."):
         config = pre_checks(token, user, args)
-    # Set valid alert interval, make it carry on.
-    args.alert_interval = 3600
 
+
+def test_pre_checks_bad_check_interval():
+    # Valid test data.
+    token = "abcdefghijklmnopqrstuvwxyz1234"
+    user = "abcdefghijklmnopqrstuvwxyz1234"
+    args = Namespace(
+        threshold=1,
+        alert_interval=3600,
+        check_interval=300,
+        reduced_sensitivity=False,
+        ttl=14400,
+    )
     # Check interval not an integer.
+    args.check_interval = "moo"
     with pytest.raises(TypeError, match="Check interval must be an integer."):
         config = pre_checks(token, user, args)
     # Check interval out of range.
     args.check_interval = 100
     with pytest.raises(ValueError, match="Check interval must be >= 180."):
         config = pre_checks(token, user, args)
-    # Set valid check interval, make it carry on.
-    args.check_interval = 300
 
+
+def test_pre_checks_bad_ttl():
+    # Valid test data.
+    token = "abcdefghijklmnopqrstuvwxyz1234"
+    user = "abcdefghijklmnopqrstuvwxyz1234"
+    args = Namespace(
+        threshold=1,
+        alert_interval=3600,
+        check_interval=300,
+        reduced_sensitivity=False,
+        ttl=14400,
+    )
     # TTL interval not an integer.
+    args.ttl = "moo"
     with pytest.raises(TypeError, match="TTL must be an integer."):
         config = pre_checks(token, user, args)
     # TTL out of range.
@@ -75,20 +164,19 @@ def test_pre_checks():
     args.ttl = 31536001
     with pytest.raises(ValueError, match="TTL must be betwen 1 and 31536000."):
         config = pre_checks(token, user, args)
-    # Set valid TTL, make it carry on.
-    args.ttl = 14400
 
-    # Check returned dict. Can use for
-    config = pre_checks(token, user, args)
-    assert config == {
-        "token": "moo",
-        "user": "moo",
-        "threshold": 3,
-        "alert_interval": 3600,
-        "check_interval": 300,
-        "reduced_sensitivity": False,
-        "ttl": 14400,
-    }
+
+def test_pre_checks_bad_ttl():
+    # Valid test data.
+    token = "abcdefghijklmnopqrstuvwxyz1234"
+    user = "abcdefghijklmnopqrstuvwxyz1234"
+    args = Namespace(
+        threshold=1,
+        alert_interval=3600,
+        check_interval=300,
+        reduced_sensitivity=False,
+        ttl=14400,
+    )
 
 
 def test_should_alert():
